f52f872c523657bf3a14241f89a9812ad46c985d
Wesley Johanson <wesleyjohanson@Wesleys-MacBook-Pro.local>
2020-09-07 18:03:26 -0700

    finished the leetcode problem, at least the majority of it. Will have to test for edge cases

--- leetcode/week01/1408_sumOf1DArray_0.java
+++ leetcode/week01/1408_sumOf1DArray_0.java
@@ -6,7 +6,13 @@ class Solution{
 		System.out.println("length = " + nums.length);
 		for (int i = 0 ; i < nums.length ; i++){
 			System.out.printf("for loop i = %d\n", i);
-			total[0] += nums[i];
+			if (i > 0){
+			System.out.println("loop 1***");
+			total[i] += nums[i] + total[i - 1];
+			}else{
+			System.out.println("loop 2**");
+			total[i] += nums[i];
+			}
 		}
 		System.out.println("Reached the end of the loops");
 		return total;
@@ -16,12 +22,12 @@ class Solution{
 		int[] nums = {1, 2, 4, 5 };
 		int[] total = runningSum(nums);
 		System.out.print("The result is [");
-		for (int i = 0 ; i < nums.length ; i++){
-			System.out.printf("%i", i);
-			System.out.printf("%s", (i == total.length - 1) ? "" :
-				", ");
-			}
+		for (int i = 0; i < total.length; i++)
+		{
+			System.out.printf("%d",total[i]); 
+			if (i != total.length - 1)
+				System.out.print(", ");
+		}
 		System.out.println("]");
-
 	}
 }
