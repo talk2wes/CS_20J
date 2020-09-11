5f73fea426e9c0da20997392ef50ba2ebea68727
Wesley Johanson <wesleyjohanson@Wesleys-MacBook-Pro.local>
2020-09-10 19:03:14 -0700

    I got the program to measure the length of strings and to put them into an int array.

--- Assignments/Words.java
+++ Assignments/Words.java
@@ -26,25 +26,30 @@ public class Words{
 		//Make that the start of the string
 		StringBuilder str = new StringBuilder();
 		str.append((char) a);
-		System.out.printf("The first letter is : '%s' from '%c'\n", str.toString(), a);
-		System.out.printf("Str_generator: \t\t\t%c", a);
+		//System.out.printf("The first letter is : '%s' from '%c'\n", str.toString(), a);
+		//System.out.printf("Str_generator: \t\t\t%c", a);
 		while (a != 'Z' + 1){
 			a = num.Randomize('A', 'Z' + 1);
 			if (a != 'Z' + 1){
 				str.append((char) a);
-				System.out.printf("%c", (char) a);
+				//System.out.printf("%c", (char) a);
 			}
 		}
-		System.out.printf("\nStringBuilder return: \t%s\n\n", str.toString());
+		//System.out.printf("\nStringBuilder return: \t%s\n\n", str.toString());
 		return (str);
 	}
 
 	public static void main(String[] args){
 		int num_of_string = 20; //  (int) (Math.pow(10,6));
+		int[] str_len = new int[num_of_string];
+
 		ArrayList<String> arrlst = new ArrayList<String>(1000);
 		for (int i = 0 ; i < num_of_string ; i++){
 			Words new_str = new Words();
-			//System.out.println(" TEST xx");
+			System.out.printf("new string = %s\n", new_str.str);
+			String temp_str = new_str.str.toString();
+			str_len[i] = temp_str.length();	
+			System.out.printf("length: \t%d\n",str_len[i]);
 		}
 		System.out.printf("Number of strings = %d\n", num_of_string);
 	}
