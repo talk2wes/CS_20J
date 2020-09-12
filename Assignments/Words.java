2cb856c737b1d3e00459e5c13d3dca9f7654e17b
Wesley Johanson <wesleyjohanson@Wesleys-MacBook-Pro.local>
2020-09-12 14:30:00 -0700

    added the javadoc documentation and everything seems to be working well

--- Assignments/Words.java
+++ Assignments/Words.java
@@ -1,24 +1,46 @@
-/*	Wesley Johanson
-	wjohanso
-	talk2wes@gmail.com
-	Words.java
-	Programming Assignment #1 Part 1
-
-	Status: WRITE THE STATUS HERE BEFORE TURNING IN!!!!!!!
-*/
-
 import java.util.ArrayList;
 import java.util.Random; 
 import java.lang.Math;
 import java.lang.StringBuilder;
 
-public class Words{
-	StringBuilder str;	
+/**
+ * CS20J-1-115304 Programming Assignment #1 Part 1
+ * Filename: Words.java
+ * This program is designed to generate a series of strings
+ * specified by NUM_STRING and stores them into an Array List named
+ * arrlst. The length of each string in arrlst is stored into str_len. 
+ * The average string length is then calculated using the method Average
+ * of the Words class. The average length is then output to std-out.
+ * Working/Untested. Code Compiles, issues are undetermined
+ * 
+ * @Author Wesley Johanson, talk2wes@gmail.com, Pengo: wjohanso
+ * @Version: 1.0
+ * @Since Fall 2020 Semester. Java 14.0.2 2020-07-14
+ */
 
+public class Words{
+	/**
+	 * This will store the randomly generated string output from the
+	 * str_generator function.
+	 */
+	StringBuilder str;	
+	
+	/**
+	 * This stores a randomly generated string into private member variable
+	 * str.
+	 */
 	public Words(){
 		str = str_generator();
 	}
-
+	
+	/**
+	 * This outputs a randomly generated string of capital letters, with
+	 * minimum length of 1. All letters have equal pseudo-random chance of
+	 * being selected (1/26). Once the first letter is choosen, there is a
+	 * probability (p=1/27) that a letter or end of string will be choosen.
+	 * 
+	 * @return	str	Randomly generated string
+	 */
 	StringBuilder str_generator(){
 		RandomNum num = new RandomNum();
 		int a = num.Randomize('A', 'Z');
@@ -32,6 +54,14 @@ public class Words{
 		}
 		return (str);
 	}
+
+	/**
+	 * This calculated the average length of all of the strings stored in 
+	 * the int array named nums.
+	 * 
+	 * @param 	nums	int array that stores the length of each string
+	 * @return		the average length of all string lengths
+	 */
 	double Average(int[] nums){
 		double total = 0;
 		for (int i = 0; i < nums.length; i++){
@@ -40,6 +70,12 @@ public class Words{
 		return ( total / nums.length);
 	}
 
+	/**
+	 * Given the parameter NUM_STRING, the loop generates the specified
+	 * number of strings, stores the length of each string into str_len and
+	 * then calls the method average to find the average length of all the
+	 * strings.
+	 */
 	public static void main(String[] args){
 		final int num_of_string = (int) (Math.pow(10,6));
 		int[] str_len = new int[num_of_string];
@@ -57,7 +93,19 @@ public class Words{
 }
 
 class RandomNum{
+	/**
+	 * This is the randomly generated number that will be returned.
+	 */
 	public int num;
+
+	/**
+	 * This generates a random number within the range: [min, max].
+	 * (inclusive of the boundaries)
+	 * 
+	 * @param	min	the beginning of the range
+	 * @param	max	the end of the range
+	 * @return	num	the randomly generated number
+	 */
 	int Randomize(int min, int max){
 		Random rng = new Random();
 		int num;
