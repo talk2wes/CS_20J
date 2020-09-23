8c497ff01ae47c2a541ca168b688881dc1783c4e
Wesley Johanson <wesleyjohanson@Wesleys-MacBook-Pro.local>
2020-09-23 11:01:17 -0700

    Have pretty much finished the output. I think there is something wrong with my standard deviation calulation though.

--- Assignments/assignment1Complete/Words.java
+++ Assignments/assignment1Complete/Words.java
@@ -4,8 +4,8 @@
  * This program is designed to generate a series of strings
  * specified by NUM_STRING and stores them into an Array List named
  * arrlst. The length of each string in arrlst is stored into strLen. 
- * The average string length is then calculated using the method average
- * of the Words class. The average length is then output to std-out.
+ * The mean string length is then calculated using the method mean
+ * of the Words class. The mean length is then output to std-out.
  * Working/tested. Code Compile, works as expected.
  * @Author Wesley Johanson, talk2wes@gmail.com, Pengo: wjohanso
  * @Version: 1.0
@@ -37,13 +37,13 @@ public class Words{
 	}
 
 	/**
-	 * This calculated the average length of all of the strings stored in 
+	 * This calculated the mean length of all of the strings stored in 
 	 * the int array named strLen.
 	 * 
 	 * @param 	strLen	int array that stores the length of each string
-	 * @return		the average length of all string lengths
+	 * @return		the mean length of all string lengths
 	 */
-	public static double average(int[] strLen){
+	public static double mean(int[] strLen){
 		double total = 0;
 		for (int i = 0; i < strLen.length; i++)
 			total += strLen[i];
@@ -56,24 +56,30 @@ public class Words{
 	}
 	
 	public static double stdDev(int strLen[]){
-		double		average = Words.average(strLen);
+		double		mean = Words.mean(strLen);
 		double		rieSum = 0;
 		
 		for (int i = 0 ; i < strLen.length ; i++)
-			rieSum += Math.pow(strLen[i] - average, 2);
-		return (Math.pow(rieSum / strLen.length , 1/2));
+			rieSum += Math.pow(strLen[i] - mean, 2);
+		rieSum /= strLen.length;
+		return (Math.pow(rieSum, 1/2));
 	}
 
 	/**
 	 * Given the const NUM_OF_STRING, the loop generates the specified
 	 * number of strings, stores the length of each string into strLen and
-	 * then calls the method average to find the average length of all the
+	 * then calls the method mean to find the mean length of all the
 	 * strings.
 	 */
 	public static void main(String[] args){
 		final int 	NUM_OF_STRING = (int) (Math.pow(10,6));
 		final int	MAX_USER_INPUT = 79242939;
 		int		userInput = 0;
+		final String	line1;
+		line1 = "CS 20J Program #1: Random Strings   "
+			+ "Wesley Johanson Talk2wes@gmail.com\n"
+			+ "Generates N strings (10^6 w/o parameter) and stats "
+			+ "about the dataset\n";
 		
 		//DETERMINE HOW MANY STRINGS TO PROCESS 
 		if (args.length == 1 
@@ -83,16 +89,24 @@ public class Words{
 		else
 			userInput = NUM_OF_STRING;
 		//exceptions are thrown for large +/- numbers	
+		//declare the array and arraylist
 		int[]		strLen = new int[userInput];
 		ArrayList<String> arrlst = new ArrayList<String>(userInput);
+		//Gen strings and store them	
 		for (int i = 0 ; i < userInput ; i++){
 			arrlst.add(i, Words.strGenerator());
 			strLen[i] = (arrlst.get(i)).length();
 		}
-		System.out.printf("CS 20J Program #1 random strings\n"
-				+ "");
-		System.out.printf("%.1f mean length\n",
-					Words.average(strLen));
-		System.out.printf("%.1f std dev\n", Words.stdDev(strLen));
+		System.out.printf(line1);
+		System.out.printf("%d random strings generated\n"
+				+ "first string: %s\n"
+				+ "last string: %s\n"
+				+ "%.1f mean length\n"
+				+ "%.1f std dev\n",
+				userInput, 
+				arrlst.get(0),
+				arrlst.get(userInput - 1),
+				Words.mean(strLen),
+				Words.stdDev(strLen));
 	}
 }
