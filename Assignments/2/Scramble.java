76c9ac1e174808d9092b13c139b74cacb86d330c
Wesley Johanson <wesleyjohanson@Wesleys-MacBook-Pro.local>
2020-09-28 09:51:22 -0700

    got the str2vec function to start working, now it takes each word and stores it into a vector

--- Assignments/2/Scramble.java
+++ Assignments/2/Scramble.java
@@ -1,3 +1,59 @@
-
+import java.util.Vector;
+import java.lang.String;
+import java.util.Scanner;
 
 //Here we go again haha
+
+
+class Scramble
+{	
+
+	public static Vector<String> str2vec(Scanner s)
+	{
+		Vector<String> words = new Vector<String>();
+		//Scanner s = new Scanner(System.in);
+		String input = "";
+		boolean done = false;
+		while (!done)
+		{
+			try {
+				input = s.next();
+				//System.out.println(input);
+				words.add(input);
+				System.out.println("Words added = " + words.get(words.size() - 1));
+			}
+			catch (Exception e){
+				done = true;
+				s.close();
+			}
+		}
+		return (words);
+	}
+
+	public static void main(String[] args) 
+	{
+
+		//Read the input from standard input 
+		Scanner s = new Scanner(System.in);
+		Vector<String> words = new Vector<String>();
+		words = str2vec(s);
+		System.out.println(words.toString());
+		/*
+		String input = "";
+		boolean done = false;
+		while (!done)
+		{
+			try {
+				input = s.next();
+				System.out.println(input);
+				//System.out.println("Next() = \t" + s.hasNext());
+			}
+			catch (Exception e){
+				done = true;
+				s.close();
+			}
+		}
+		System.out.println("LOOP TERMINATED"); 			//TESTING 
+		*/
+	}
+}
