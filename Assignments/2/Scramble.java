1e46cce78a52fd3e9b605ff16e4dcb785e7f617e
talk2wes <talk2wes@gmail.com>
2020-10-04 19:26:59 -0700

    removed the debugging output (almost all). Now I'm debugging the input to see exactly what the instructor wants

--- Assignments/2/Scramble.java
+++ Assignments/2/Scramble.java
@@ -14,21 +14,17 @@ class Scramble
 		boolean done = false;
 		while (!done)
 		{
-			//System.out.println("1x");
 			try {
-				input = s.nextLine();
+				input = s.next();
+				System.out.println("done with input");
 				words.add(input);
-				//System.out.println("2x input[0]: " + input.charAt(0));
 			}
 			catch (Exception e){
+				System.out.println("EOF reached");
 				//Catched the exception as a result of EOF input
 				done = true;
-				System.out.println("");
-				//System.out.println("3x");
 			}
 		}
-		//System.out.println("RETURN str2vec");
-		//System.out.println("");
 		return (words);
 	}
 
@@ -47,7 +43,6 @@ class Scramble
 			//Split each string into an int array using the delimiter
 			tempStr = lines.get(row);			
 			words = tempStr.split(delimiter);
-			//System.out.println(tempStr);		//Testing
 			//Add each word as an element of the vector 
 			Vector<String> splitStr = new Vector<String>();
 			for (int col = 0; col < words.length; col++)
@@ -65,7 +60,6 @@ class Scramble
 
 		for (int i = 0; i < str.length(); i++)
 		{
-			//System.out.println(str + "\ti: " + i + "\tchar: " + str.charAt(i));
 			if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')
 				letters++;
 			if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
@@ -78,23 +72,9 @@ class Scramble
 	public static String swapChar(String word, int i, int j)
 	{
 		StringBuilder word_sb = new StringBuilder(word);
-		//System.out.println("A: word: \"" + word + "\" word_sb: \"" + word_sb.toString()
-		//	+ "\"\t'" + word.charAt(i) + "' '" + word.charAt(j) + "'");
-		
+
 		word_sb.replace(i, i + 1, word.substring(j, j+1));
-		
-		//System.out.println("swapChar, swapped string: " + word_sb.toString());
-		//System.out.println("B: word: \"" + word + "\" word_sb: \"" + word_sb.toString()
-		//	+ "\"\t'" + word.charAt(i) + "' '" + word.charAt(j) + "'");
-		
 		word_sb.replace(j, j + 1, word.substring(i, i+1));
-		
-		//System.out.println("C: word: \"" + word + "\" word_sb: \"" + word_sb.toString()
-		//	+ "\"\t'" + word.charAt(i) + "' '" + word.charAt(j) + "'");
-		
-		//System.out.println("D: word: \"" + word + "\" word_sb: \"" + word_sb.toString()
-		//	+ "\"\t'" + word.charAt(i) + "' '" + word.charAt(j) + "'");
-		//System.out.println("swapChar, swapped string: " + word_sb.toString());
 		return(word_sb.toString());
 	}
 
@@ -111,21 +91,9 @@ class Scramble
 		{
 			//make a random number within the range [start, i)
 			swapInd = rnd.nextInt(i - start) + start;
-			//System.out.println("swapInd range: [" + start
-			//	+ ", " + (i-1) + "]" + " i = " + i);
 			//swap that index with the terminal character
-			//System.out.println("random int Choosen: " + swapInd);
-			//System.out.println(" PRE swapped string: " + word);
 			word = swapChar(word, swapInd, i);
-			//System.out.println("POST swapped string: " + word);
-			
-			
-			//System.out.println("");
-			
-
-
 		}
-		//System.out.println("POST swapped string: " + word);
 		return (word);
 	}
 
@@ -138,18 +106,14 @@ class Scramble
 			for (int col = 0; col < mat.get(row).size(); col++)
 			{
 				//Find length of string, excluding punctuation
-				//System.out.println("\n\n");
-				//System.out.println(mat.get(row).get(col) + "\tlen: " + wordLen(mat.get(row).get(col)));
 				len = wordLen(mat.get(row).get(col));
 				if (len > 3)
 				{
-					//System.out.println("SCRAMBLE THIS WORD");
 					mat.get(row).set(col, 
 						wordShuffle(mat.get(row).get(col), 1, 
 						wordLen(mat.get(row).get(col)) - 2)); //one for index difference, one for 
 					//Given length define the range of indicies to scramble
 					//Scramble the letters within the range 
-					//System.out.println("post scramble: " + mat.get(row).get(col));
 				}
 			}
 		}
@@ -178,13 +142,9 @@ class Scramble
 		Vector<String> lines = new Vector<String>();
 		Vector<Vector<String>> mat = new Vector<Vector<String>>();
 		
-		//System.out.println("\nstr2vec\n");
 		lines = str2vec(s, "\n");
-		//System.out.println("");
 		mat = vec2matrix(lines, " ");
 		scramble(mat);
-		//System.out.println("mat = " + mat.toString() + "\n");
-		//System.out.println("\nmat2stdout\n");
 		mat2stdOut(mat);
 	}
 }
