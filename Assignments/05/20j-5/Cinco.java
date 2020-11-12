b7c4dc0b02b5349b8048643e1e08b9ec59ffed3b
Wesley Johanson <talk2wes@gmail.com>
2020-11-11 20:02:38 -0800

    got the count matching letters function working, no deep bug testing though

--- Assignments/05/20j-5/Cinco.java
+++ Assignments/05/20j-5/Cinco.java
@@ -9,10 +9,11 @@ import java.io.*;
 class Dictionary{
 	private HashSet<String> words;
 	private ArrayList<String> secrets;
-	private static Random rng = new Random();
+	private Random rng;
 
-// constructor: read words from a file   
+	// constructor: read words from a file   
 	public Dictionary(String filename){
+		rng = new Random();
 		words = new HashSet<String>();
 		secrets = new ArrayList<String>();
 		String temp = "";
@@ -46,6 +47,7 @@ class Dictionary{
 
 	// is this word a legal secret word?
 	private boolean isLegalSecretWord(String word){
+
 		for (int i = 0; i < word.length(); i++){
 			for (int j = 0; j < word.length(); j++)
 				if (i != j && word.charAt(i) == word.charAt(j))
@@ -56,33 +58,57 @@ class Dictionary{
 }
 
 class Cinco{
-	//Dictionary dictionary;
 	int numguesses;
-	boolean cheated;
-	String secret;
+	boolean cheated = false; //innocent until proven guilty
+	String secret ;
 	private static String DEFAULT_WORD_LIST = "cinco-words.txt";
+	Dictionary dictionary;
 
 	public Cinco(){ }
 	public Cinco(String filename){ }
-	public void play(){ } // contains main game loop and console i/o
+	public void play(){
+		dictionary  =  new Dictionary(DEFAULT_WORD_LIST);	
+		Scanner input = new Scanner(System.in);
+		secret = dictionary.getLegalSecretWord();
+
+		System.out.println("Secret word : " + secret);
+		while (input.hasNextLine()){
+			String myInput = input.nextLine();
+			System.out.println("valid word? = " + 
+				dictionary.validWord(myInput) + 
+				"\nCount Matching Letters = " + 
+				countMatchingLetters(myInput) );
+			
+		}
+
+			
+	} // contains main game loop and console i/o
 
 	// return # of matching letters secret/guess
-	//private int countMatchingLetters(String guess){ }
+	private int countMatchingLetters(String guess){
+		int diffs = 0;
+		int lettersInAlpha = 26;
+		boolean alphabet[] = new boolean[lettersInAlpha];
+		
+		if (secret.length() != guess.length())
+			return -1;
+		for (int i = 0; i < guess.length(); i++)
+			for (int j = 0; j < guess.length(); j++)
+				if (secret.charAt(i) == guess.charAt(j))
+					alphabet[i] = true;
+		for (int i = 0; i < lettersInAlpha; i++)
+			diffs = alphabet[i] ? diffs + 1 : diffs;
+		return diffs;
+	}
+
+	
 
 	// return # of in-place letters secret/guess
 	//private int countInPlaceLetters(String guess){ }
 
 	// main program, required, minimal code
 	public static void main(String [] args){
-		//TESTING
-		Dictionary dictionary = new Dictionary(DEFAULT_WORD_LIST);
-		if (args.length > 0)
-			System.out.println("args[0] =" + args[0]);
-		//TESTING LOOP
-		Scanner input = new Scanner(System.in);
-		while (input.hasNextLine()){
-			System.out.println("rng = " + dictionary.getLegalSecretWord());
-			System.out.println("validWord?:" + dictionary.validWord(input.nextLine()));
-		}
-	}
+		Cinco game = new Cinco();
+		game.play();
+	}		
 }
