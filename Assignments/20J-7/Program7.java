d2c78ade88936642f2ec0a9a4086193f5a691c4e
Wesley Johanson <talk2wes@gmail.com>
2020-12-16 20:56:48 -0800

    working on triple cut

--- Assignments/20J-7/Program7.java
+++ Assignments/20J-7/Program7.java
@@ -9,13 +9,9 @@ import java.lang.StringBuilder;
 
 class Program7{
 
-	final int JOKER_A = 27;
-	final int JOKER_B = 28;
-	
-	public static List readDeckFile(Scanner deckFile){
-		List deck = new List();
-		return deck;
-	}
+	static final int JOKER_A = 27;
+	static final int JOKER_B = 28;
+	static final int LET_IN_ALPHA = 26;
 	
 	//string to ascii vals 
 	public static int[] strToIntArr(String msg){
@@ -27,7 +23,7 @@ class Program7{
 		return asciiValues;
 	}
 
-	// ascii to string 
+	//converts int array of ascii values to string 
 	public static String intArrToStr(int[] asciiValues){
 		StringBuilder str = new StringBuilder();
 		for (int i = 0; i < asciiValues.length ; i++){
@@ -36,47 +32,37 @@ class Program7{
 		return str.toString();
 	}
 
+	//encrypt the message
 	public static String encrypt(String msg, List deck){
-		String encrypted = "";
 		int[] asciiValues = strToIntArr(msg);
-		int[] keystreamValues = genKeystream(deck, msg.length());
-		
-		encrypted = intArrToStr(asciiValues);	
-		return encrypted ; //testing  
+		int[] keystreamValues = deck.genKeystream(msg.length());
+		int[] encrypted = new int[msg.length()];	
+		for (int i = 0; i < msg.length(); i++){
+			encrypted[i] = asciiValues[i] + keystreamValues[i];
+			encrypted[i] = encrypted[i] % LET_IN_ALPHA;
+			encrypted[i] += (int) 'A';
+		}
+		return intArrToStr(encrypted); 
 	}
 
+	//decrpt the message 
 	public static String decrypt(String msg, List deck){
 		String decrypted = "lil pussy";
 		return decrypted;
 	}
-	
+
+	//Convert the message to plaintext (only uppercase letters)
 	public static String plaintext(String msg){
 		StringBuilder plaintext = new StringBuilder();
-		for (int i = 0; i < msg.length() ; i++){
-			char msgChar = msg.charAt(i);
-			if (Character.isLetter(msgChar))
-				plaintext.append(msgChar);
-		}
-		//System.out.println("length = " + plaintext.length());
+		//Add all alphabetical characters
+		for (int i = 0; i < msg.length() ; i++)
+			if (Character.isLetter(msg.charAt(i)))
+				plaintext.append(msg.charAt(i));
+		//add padding to make string lenth a multiple of 5
 		int paddingChars =  5 - plaintext.length() % 5;
-		for (int i = 0; i < paddingChars ; i++)
-			plaintext.append("X");
+		for (int i = 0; i < paddingChars ; i++)	plaintext.append("X");
+		//convert to upper case and return
 		return (plaintext.toString()).toUpperCase();
-	}
-
-	public static int[] genKeystream(List deck, int numOfValues){
-		int[] keystreamValues = new int[numOfValues];
-		return keystreamValues;
-	}
-	
-	public static int genKeystream(List deck){
-		//step 1 
-		//step 2
-		//step 3
-		//step 4
-		//step 5
-
-		return -1;
 	}
 
 	public static void main(String args[]){
@@ -84,10 +70,13 @@ class Program7{
 		if ( args.length != 2) return ;
 
 		try {
-			//read deck from file
+			//Load the deck from the deckFile 
 			Scanner deckFile = new Scanner( new File( args[1] ) );
 			Scanner input = new Scanner(System.in);
 			List deck = new List();
+			deck = List.readDeckFile(deckFile);
+			//System.out.println("deck.toString() = " + deck.toString());
+			deckFile.close();
 
 			//encrypt message
 			if ( args[0].equals("e") ){
@@ -112,19 +101,192 @@ class Program7{
 }
 
 class List{
-	private LinkNode head;
-	private LinkNode tail;
+	protected LinkNode head;
+	protected LinkNode tail;
+	static final int JOKER_A = 27;
+	static final int JOKER_B = 28;
+	static final int LET_IN_ALPHA = 26;
 
 	List(){ head = tail = null; }
 
+	public int[] genKeystream(int numOfValues){
+		int[] keystreamValues = new int[numOfValues];
+		for (int i = 0; i < numOfValues; i++)
+			keystreamValues[i] = this.genKeystream(); // testing 
+		return keystreamValues;
+	}
+	
+	public static List readDeckFile(Scanner deckFile){
+		List deck = new List();
+		String[] numbers = (deckFile.nextLine()).split(" ");
+		for (String num : numbers){
+			deck.addNodeBack(Integer.parseInt(num));
+			//System.out.println(Integer.parseInt(num) + 
+			//	"\ntoString: " + deck.toString()); //testing 
+			
+		}
+		return deck;
+	}
+	
+	public String toString(){
+		StringBuilder s = new StringBuilder();
+		if (head == null)
+			return null;
+		LinkNode temp = head;
+		while (temp != null){
+			s.append(temp.data);
+			s.append(" ");
+			//System.out.println("deck.toString() = " + deck.toString());
+			temp = temp.next;
+		}
+		return s.toString();
+	}
+
+	//Swaps the node "swap" with the node that is next 
+	public void swapNext(LinkNode swap){
+		//calculate the size of the list
+		int size = 0;
+		LinkNode temp = head;
+		while (temp != null){ size++; temp = temp.next;	}
+
+		//Undefined operation: ERROR 
+		if (swap == null || size < 2 ) return ;
+	
+		//put a handle on the next node to swap with
+		LinkNode swapNext = swap.next;
+
+		//Case: swapNext exists
+		if (swapNext != null){
+			//make references to the nodes before and after the swap nodes
+			LinkNode left = swap.prev;
+			LinkNode right = swapNext.next;
+		
+			//link the two swap nodes together
+			swapNext.next = swap;
+			swap.prev = swapNext;
+
+			//Connect the swap-nodes to the rest of the list, if they exist
+			//reassign head & tail if they changed 
+			swapNext.prev = left;
+			if (left != null) 
+				left.next = swapNext;
+			else
+				head = swapNext;
+			swap.next = right;
+			if (right != null) 
+				right.prev = swap;
+			else
+				tail = swap;
+		}else{ //Case: swapNode doesn't exist. Swap with head instead
+			swapNext = head;
+			if (size == 2){ 
+				tail = swapNext.next = swap;
+				head = swap.prev = swapNext;
+				swapNext.prev = swap.next = null;
+			}else if (size == 3){
+				LinkNode body = swapNext.next;
+				//link the body to the terminal nodes 
+				tail = body.next = swap;
+				head = body.prev = swapNext;
+				//Link the terminal nodes to the body
+				swapNext.next = swap.prev = body;
+				//set the ends of the list to null
+				swapNext.prev = swap.next = null;
+			}else if (size > 3) {
+				//Make references to terminal ends of the body
+				LinkNode left = head.next;
+				LinkNode right = tail.prev;
+				
+				//link the body to the terminal nodes
+				//reassign head & tail
+				head = left.prev = swap;
+				tail = right.next = swapNext;
+				
+				//link the swap-nodes to the body
+				swap.next = left;
+				swapNext.prev = right;
+
+				//set the new ends to null
+				swap.prev = swapNext.next = null;
+			}
+		}
+		return;
+	}
+	public void tripleCut(){
+		//find the first joker
+		LinkNode jokerL = head;
+		while (jokerL != JOKER_A && jokerL != JOKER_B)
+			jokerL = jokerL.next;
+		//find the second joker
+		LinkNoder jokerR = jokerL.next
+		while (jokerR != joker_A && jokerR ! = JOKER_B && jokerR != null)
+			jokerR = jokerR.next;
+		
+
+
+
+	}
+
+	public int genKeystream(){
+		System.out.println("Before keystream:\t" + this.toString() );
+		//step 1 
+		LinkNode joker = this.head;	
+		//find the joker
+		while (joker.data != JOKER_A)
+			joker = joker.next;
+		if (joker == null) return -1; //error 
+		swapNext(joker);	
+		System.out.println("step 1:\t\t\t" + this.toString());
+
+
+		//step 2
+		joker = this.head;
+		while (joker.data != JOKER_B)
+			joker = joker.next;
+		if (joker == null) return -1; // error 
+		swapNext(joker);
+		swapNext(joker);
+		System.out.println("step 2:\t\t\t" + this.toString());
+
+
+		//step 3
+		
+		//step 4
+		//step 5
+		System.out.println("");
+
+		return -1;
+	}
 	//add node to tail end of the list
 	public void addNodeBack(int data){
+		LinkNode newNode = new LinkNode();
+		//empty list case
+		if (head == null && tail == null){
+			newNode.data = data;
+			head = tail = newNode;
+			return;
+		}
+		//list size == 1 case 
+		if (head == tail){
+			head.next = newNode;
+			tail = newNode;
+			newNode.data = data;
+			newNode.prev = head;
+			return;
+		}
+		//list size >= 2 case 
+		tail.next = newNode;
+		newNode.prev = tail;
+		tail = newNode;
+		newNode.data = data;
+		return;
 	}
 
-	class LinkNode{
-		private LinkNode next;
-		private LinkNode prev;
-		private int data;
+	protected class LinkNode{
+		public LinkNode(){next = prev = null; data = 0; }
+		public LinkNode getNext(){ return next; }
+		protected LinkNode next;
+		protected LinkNode prev;
+		protected int data;
 	}
 }
-
